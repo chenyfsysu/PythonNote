b6206d2139971f34da4458c0c0ce9140e81b229a
chenyfsysu <chenyfsysu@gmail.com>
2017-12-01 11:43:41 +0800

    add

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
-class A(object):
-	def call(self):
-		print 'call A'
+import inspect
 
-class B(A):
-	def call(self):
-		super(B, self).call()
-		print 'call B'
+class A1(object): pass
+class B1(A1): pass
+class C1(A1): pass
+class D1(B1, C1): pass
 
-# b = B()
-# b.call()  # call A call B
-print B.__mro__
+class A2: pass
+class B2(A2): pass
+class C2(A2): pass
+class D2(B2, C2): pass
+
+print D1.__mro__
+print inspect.getmro(D2)
